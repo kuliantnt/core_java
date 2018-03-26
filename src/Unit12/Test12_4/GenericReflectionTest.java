6234753938df4e233f0756f07d464f943cf6a77a
lianlian <kuliantnt@gmail.com>
2018-03-26 09:43:34 +0800

    2018年03月26日09:00:00

--- src/Unit12/Test12_4/GenericReflectionTest.java
+++ src/Unit12/Test12_4/GenericReflectionTest.java
@@ -1,5 +1,111 @@
 package Unit12.Test12_4;
 
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class GenericReflectionTest {
 
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0)
+            name = args[0];
+        else {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter class name (e.g. java.util.Collections): ");
+            name = in.next();
+        }
+        try {
+            Class<?> cl = Class.forName(name);
+            printClass(cl);
+            for (Method m : cl.getDeclaredMethods()) {
+                printMethod(m);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printClass(Class<?> cl) {
+        System.out.print(cl);
+        printType(cl.getTypeParameters(), "<", ",", ">", true);
+        Type sc = cl.getGenericSuperclass();
+        if (sc != null) {
+            System.out.print(" extends ");
+            printType(sc, false);
+        }
+        printType(cl.getGenericInterfaces(), " implements ", ",", ""
+                , false);
+        System.out.println();
+    }
+
+    private static void printMethod(Method m) {
+        String name = m.getName();
+        System.out.print(Modifier.toString(m.getModifiers()));
+        System.out.print(" ");
+        printType(m.getTypeParameters(), "<", ",", "> ", true);
+
+        printType(m.getGenericReturnType(), false);
+        System.out.print(" ");
+        System.out.print(name);
+        System.out.print("(");
+        printType(m.getGenericParameterTypes(), "", ",", "", false);
+        System.out.println(")");
+
+    }
+
+    private static void printType(Type[] types, String pre, String sep, String suf,
+            boolean isDefinition) {
+        if (pre.equals(" extends ") && Arrays.equals(types, new Type[]{Object.class}))
+            return;
+        if (types.length > 0) {
+            System.out.print(pre);
+        }
+        for (int i = 0; i < types.length; i++) {
+            if (i > 0) {
+                System.out.print(sep);
+            }
+            printType(types[i], isDefinition);
+        }
+        if (types.length > 0) {
+            System.out.print(suf);
+        }
+    }
+
+    private static void printType(Type type, boolean isDefinition) {
+        if (type instanceof Class) {
+            Class<?> t = (Class<?>) type;
+            System.out.print(t.getName());
+        } else if (type instanceof TypeVariable<?>) {
+            TypeVariable<?> t = (TypeVariable<?>) type;
+            System.out.print(t.getName());
+            if (isDefinition)
+                printType(t.getBounds(), " extends ", " & ", "", false);
+        } else if (type instanceof WildcardType) {
+            WildcardType t = (WildcardType) type;
+            System.out.print("?");
+            printType(t.getUpperBounds(), " extends ", " & ", "", false);
+            printType(t.getLowerBounds(), " super ", " & ", "", false);
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType t = (ParameterizedType) type;
+            Type owner = t.getOwnerType();
+            if (owner != null) {
+                printType(owner, false);
+                System.out.print(".");
+            }
+            printType(t.getRawType(), false);
+            printType(t.getActualTypeArguments(), "<", ",", ">", false);
+        } else if (type instanceof GenericArrayType) {
+            GenericArrayType t = (GenericArrayType) type;
+            System.out.print("");
+            printType(t.getGenericComponentType(), isDefinition);
+            System.out.print("[]");
+        }
+    }
 }
