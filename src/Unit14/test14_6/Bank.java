17c083cd73cc24496855324336eb57b22d1d549e
lianlian <kuliantnt@gmail.com>
2018-03-31 18:49:21 +0800

    2018-3-31

---
+++ src/Unit14/test14_6/Bank.java
@@ -0,0 +1,64 @@
+package Unit14.test14_6;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bank {
+
+    private final double[] accounts;
+    private Lock bankLock = new ReentrantLock();
+
+
+    /**
+     * 银行程序
+     * @param n 账户数量
+     * @param initialBalance 期初余额
+     */
+    public Bank(int n, double initialBalance) {
+        accounts = new double[n];
+        for (int i = 0; i < accounts.length; i++) {
+            accounts[i] = initialBalance;
+        }
+    }
+
+    /**
+     * 转账
+     * @param from 来自账户
+     * @param to 目标账户
+     * @param amount 数量
+     */
+    public void transfer(int from, int to, double amount) {
+        bankLock.lock();
+
+        try {
+            if (accounts[from] < amount)
+                return;
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+        }finally{
+            bankLock.unlock();
+        }
+    }
+
+    /**
+     * 获取全部的账户余额
+     * @return 全部余额
+     */
+    private double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts)
+            sum += a;
+        return sum;
+    }
+
+    /**
+     * 账户数量
+     * @return 数量
+     */
+    public int size() {
+        return accounts.length;
+    }
+}
---
+++ src/Unit14/test14_7/TransferRunnable.java
@@ -0,0 +1,32 @@
+package Unit14.test14_7;
+
+import Unit14.test14_6.Bank;
+
+public class TransferRunnable implements Runnable {
+
+    private Bank bank;
+    private int fromAccount;
+    private double maxAmount;
+    private int DELAY = 10;
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 1000; i++) {
+                int toAccount = (int) (bank.size() * Math.random());
+                double amount = maxAmount * Math.random();
+                bank.transfer(fromAccount, toAccount, amount);
+                Thread.sleep((long) (DELAY * Math.random()));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public TransferRunnable(Bank b, int from, double max) {
+        bank = b;
+        fromAccount = from;
+        maxAmount = max;
+    }
+}
